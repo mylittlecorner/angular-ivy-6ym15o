56cfcd89c197d9963ccd145cb9d9e31cea7bba09
mylittlecorner <83501465+mylittlecorner@users.noreply.github.com>
2022-11-28 12:31:44 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,49 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   testVariable: string;
   testVariableObservable: Observable<string>;
+  blockingTime: string;
+  unblockingTime: string;
+  constructor() {
+    this.blockingTime = '2022-11-25T03:20:00';
+    this.unblockingTime = '2022-11-25T12:02:00';
+  }
+
+  cmpTime(blkTime: Date, ublkTime: Date, crntTime: Date): boolean {
+    let blkTimeNumber =
+      blkTime.getHours() * 3600 +
+      blkTime.getMinutes() * 60 +
+      blkTime.getSeconds();
+    let ublkTimeNumber =
+      ublkTime.getHours() * 3600 +
+      ublkTime.getMinutes() * 60 +
+      ublkTime.getSeconds();
+    let currentTimeNumber =
+      crntTime.getHours() * 3600 +
+      crntTime.getMinutes() * 60 +
+      crntTime.getSeconds();
+
+    if (
+      currentTimeNumber >= blkTimeNumber &&
+      currentTimeNumber <= ublkTimeNumber
+    ) {
+      return true;
+    }
+    return false;
+  }
   ngOnInit() {
     this.testVariableObservable = new Observable((observer) => {
-      setTimeout(() => {
-        observer.next('Interesting string');
-      }, 5000);
+        let currentDate = new Date();
+        if (
+          this.cmpTime(
+            new Date(this.blockingTime),
+            new Date(this.unblockingTime),
+            currentDate
+          )
+        ) {
+          observer.next(currentDate.toTimeString());
+        } else {
+          observer.next('NOPE');
+        }
     });
   }
   onClickButton() {
@@ -22,3 +60,28 @@ export class AppComponent implements OnInit {
     });
   }
 }
+
+/*
+
+blockingTime
+: 
+"2022-11-25T03:20:00"
+categoryId
+: 
+1
+day
+: 
+1
+id
+: 
+2
+is24Hours
+: 
+false
+isActive
+: 
+true
+unblockingTime
+: 
+"2022-11-25T18:40:00"
+*/
